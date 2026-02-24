016a3472d551290ce8abfce0fd458c101873cef7
karunamin <freedom12528@gmail.com>
2026-02-24 17:30:56 +0900

    커밋

---
+++ basic/algorithm03/app02/app02.c
@@ -0,0 +1,37 @@
+﻿// app02 - 팩토리얼 재귀방식
+// result 를 ing로 하면 12!까지만 계산가능
+// unsigned long long이면 20!까지 계산가능
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+unsigned long long factorial(int n);	// int factorial(int n)과 비교
+
+int main(void) {
+	int x;
+	unsigned long long result = 0;
+	puts("팩토리얼 재귀 호출");
+
+	while (1) {
+		printf("정수 입력 > "); scanf("%d", &x);
+		if (x <= 0) break;
+
+		result = factorial(x);
+
+		printf("%d! = %llu \n", x, result);
+	}
+	return 0;
+}
+
+// factorial / 1) 0! ==1 / 2) n! ==n x (n - 1)!
+unsigned long long factorial(int n) {
+	if (n > 0) {	// n x (n - 1)!
+		// return n* factorial(n - 1); /. int 일 경우
+		return (unsigned long long)n * factorial(n - 1);	// 5 * factorial(4) -> 5 * 4 * factorial(3)...
+	}
+	else {	// 0!
+		return 1;	// 반드시 종료조건이 있어야함!
+	}
+
+}
---
+++ basic/algorithm03/app05/app05.c
@@ -0,0 +1,45 @@
+﻿// app05 - 재귀 알고리즘 비재귀로 변경
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include "IntStack.h"
+/*--- 재귀함수 호출부분 제거 ---*/
+void recur(int n) {
+	IntStack stk;	// 스택추가
+	Initialize(&stk, 100);
+	
+	
+Top: // 
+	if (n > 0) {
+		//recur(n - 1);	// 머리재귀 제거
+		Push(&stk, n);		// n값을 스택에 푸쉬
+		n = n - 1;
+		goto Top;
+	}
+	if(!IsEmpty(&stk)){
+		Pop(&stk, &n);	// 임시 저장한 n값을 팝
+		printf("%d\n", n);
+
+		//recur(n - 2); // 꼬리재귀 제거
+		n = n - 2;
+		goto Top;
+
+	}
+
+	Terminate(&stk);
+}
+
+int main(void) {
+	int x;
+
+
+	while (1) {
+		printf("정수 입력 > "); scanf("%d", &x);
+
+		if (x <= 0)break;
+
+		recur(x);
+	}
+	return 0;
+}
